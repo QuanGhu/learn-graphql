0d94a6a5f7179ffc7b99ee360f7f7122bf52f18a
Ari P <ariteknologi@gmail.com>
2021-07-30 21:51:39 +0700

    init graphql

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+let schema = mongoose.Schema({
+    name : {
+        type : String
+    },
+    is_active : {
+        type : Boolean
+    }
+}, {
+    timestamps: {
+        createdAt : 'created_at',
+        updatedAt : 'updated_at'
+    }
+})
+
+module.exports = mongoose.model("category", schema)
+
